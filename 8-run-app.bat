4bd98a6312158928a6a94e0ad519c44cb203cb82
İbrahim DEMİRCİ <ibrahimmdemircii@gmail.com>
2026-03-14 20:56:04 +0300

    fix(startup): restore GUI flag and fix mainClass mismatch

--- 8-run-app.bat
+++ 8-run-app.bat
@@ -3,7 +3,7 @@
 @cd /d "%~dp0"
 
 echo Running Application
-java -jar petreminder-app/target/petreminder-app-1.0-SNAPSHOT.jar
+java -jar petreminder-app/target/petreminder-app-1.0-SNAPSHOT.jar --gui
 
 echo Operation Completed!
 pause
